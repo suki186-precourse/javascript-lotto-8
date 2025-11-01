0ae76fb09f9a46838c19fa50dce55b484543e97a
Suki <tues1205@naver.com>
2025-11-02 03:35:55 +0900

    feat(LottoController): 비즈니스 로직 담당 컨트롤러 생성

---
+++ src/controller/LottoController.js
@@ -0,0 +1,21 @@
+import LottoGame from "../domain/LottoGame.js";
+import { InputView } from "../view/InputView.js";
+
+class LottoController {
+  #lottoGame; // LottoGame 인스턴스
+
+  async play() {
+    // ===== 1. 로또 구매 금액 처리
+    // 1,000원 단위 입력 받기
+    const purchaseAmount = await InputView.readPurchaseAmount();
+
+    // 발행 수량 계산 (LottoGame 생성)
+    this.#lottoGame = new LottoGame(purchaseAmount);
+
+    // 테스트용 출력
+    const lottoCount = this.#lottoGame.getLottoCount();
+    console.log(lottoCount);
+  }
+}
+
+export default LottoController;
