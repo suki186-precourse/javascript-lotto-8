beb4d6295247051c211acf9582ce41728bc2881c
suki186 <ksh.1205263@gmail.com>
2026-01-10 08:56:57 +0900

    refactor(controller): Controller 구조 리팩토링

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -3,31 +3,46 @@ import { InputView } from "../view/InputView.js";
 import { OutputView } from "../view/OutputView.js";
 import { Validator } from "../view/Validator.js";
 
-
 export class LottoController {
-  #lottoGame;
-
   async run() {
-        // 구입 금액 입력
-        await this.#handleInput(async() => {
-            const sumMoney = await InputView.readSumMoney();
-            this.#lottoGame = new LottoGame(sumMoney);
-        })
+    // 구입 금액 입력 및 객체 생성
+    const lottoGame = await this.#handleInput(() => this.#purchaseLotto());
 
-        // 나의 로또 목록
-        this.#printMyLottos();
+    // 당첨 및 보너스 번호 입력
+    await this.#handleInput(() => this.#settingNumbers(lottoGame));
 
-        // 당첨 번호 입력
-        const winningNumbers = await this.#readWinningNumbers();
+    // 결과 계산 및 출력
+    this.#displayResult(lottoGame);
+  }
 
-        // 보너스 번호 입력
-        const bonusNumber = await this.#readBonusNumber(winningNumbers);
+  async #purchaseLotto() {
+    const sumMoney = await InputView.readSumMoney();
+    const game = new LottoGame(sumMoney);
 
-        // lottoGame 데이터 저장
-        this.#lottoGame.setWinningNumbers(winningNumbers, bonusNumber);
-        
-        // 당첨 통계 계산 및 출력
-        this.#printResult();
+    // 로또 생성 및 출력
+    game.createLottos();
+
+    OutputView.printLottoCount(game.lottoCount);
+    OutputView.printMyLottos(game.lottos);
+
+    return game;
+  }
+
+  async #settingNumbers(lottoGame) {
+    const winningNumbers = await InputView.readWinningNumbers();
+    const bonusNumber = await InputView.readBonusNumber();
+    Validator.validateBonusDuplicate(winningNumbers, bonusNumber);
+
+    lottoGame.setWinningNumbers(winningNumbers, bonusNumber);
+  }
+
+  #displayResult(lottoGame) {
+    const result = lottoGame.drawResult();
+    OutputView.printLottoResult(result);
+
+    const totalPrize = lottoGame.drawTotalPrize(result);
+    const returnRate = lottoGame.drawReturnRate(totalPrize);
+    OutputView.printReturnRate(returnRate);
   }
 
   // 재입력 로직
@@ -35,45 +50,9 @@ export class LottoController {
     while (true) {
       try {
         return await inputAction();
-        break;
       } catch (error) {
         OutputView.printError(error);
       }
     }
-  }
-
-  async #readWinningNumbers() {
-    return await this.#handleInput(async () => {
-        return await InputView.readWinningNumbers();
-    });
-  }
-
-  async #readBonusNumber(winningNumbers) {
-    return await this.#handleInput(async () => {
-        const bonusNumber = await InputView.readBonusNumber();
-        // 당첨 번호와 중복 검사
-        Validator.validateBonusDuplicate(winningNumbers, bonusNumber);
-
-        return bonusNumber;
-    });
-  }
-
-  #printMyLottos() {
-    const count = this.#lottoGame.lottoCount;
-    OutputView.printLottoCount(count)
-
-    this.#lottoGame.createLottos();
-
-    const lottos = this.#lottoGame.lottos;
-    OutputView.printMyLottos(lottos);
-  }
-
-  #printResult() {
-    const result = this.#lottoGame.drawResult();
-    OutputView.printLottoResult(result);
-
-    const totalPrize = this.#lottoGame.drawTotalPrize(result);
-    const returnRate = this.#lottoGame.drawReturnRate(totalPrize);
-    OutputView.printReturnRate(returnRate);
   }
 }
